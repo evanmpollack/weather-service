568bedc5b230587f1a9062cfee626869c88a2cc4
evanmpollack <evanmpollack@gmail.com>
2023-11-11 23:55:39 +0000

    UriBuilder init

---
+++ src/main/java/com/evanm/weather/geocoding/service/UriBuilder.java
@@ -0,0 +1,17 @@
+package com.evanm.weather.geocoding.service;
+
+public class UriBuilder {
+    private StringBuilder stringBuilder;
+
+    public UriBuilder(StringBuilder stringBuilder) {
+        this.stringBuilder = stringBuilder;
+    }
+
+    /**
+     * get/setProtocol
+     * get/setHost --> multiple parts? .
+     * get/setPath
+     * get/setParams(K,V)
+     * get/setFragment
+     */
+}
