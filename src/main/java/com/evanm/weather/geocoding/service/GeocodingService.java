641dec4c0cd5910e2a69a62a41be69a2c4f392a9
evanmpollack <evanmpollack@gmail.com>
2023-11-16 02:25:16 +0000

    abstracted logic, dumb service achieved

--- src/main/java/com/evanm/weather/geocoding/service/GeocodingService.java
+++ src/main/java/com/evanm/weather/geocoding/service/GeocodingService.java
@@ -1,69 +1,38 @@
 package com.evanm.weather.geocoding.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.evanm.weather.HereProperties;
 import com.evanm.weather.geocoding.dto.Address;
 import com.evanm.weather.geocoding.dto.Point;
 
 @Service
 public class GeocodingService {
-    @Autowired
-    private HereProperties hereProperties;
-    
     private RestTemplate restTemplate;
+    private HereUriComponentDirector uriComponentDirector;
 
-    public GeocodingService(RestTemplateBuilder builder) {
+    public GeocodingService(RestTemplateBuilder builder, HereUriComponentDirector director) {
         restTemplate = builder.build();
+        uriComponentDirector = director;
     }
     
-    public Point encode(String address) {
-        // Here URL builder class needed
-        // StringBuilder instead of manual string construction?
-        String service = HereApiService.GEOCODING.service;
-        String protocol = "https";
-        String host = service + "." + hereProperties.getBaseOrigin();
-        String path = hereProperties.getApiVersion() + "/" + service;
-        // find way to abstract query param names
-        String params = "q=" + address + "&apiKey=" + hereProperties.getApiKey();
-        String url = protocol + "://" + host + "/" + path + "?" + params;
-
-        try {
-            ResponseEntity<Point> res = restTemplate.getForEntity(url, Point.class);
-            if (res.getStatusCode().isError()) { throw new Exception("callout to Here API failed"); }
-            
-            return res.getBody();
-        } catch (Exception e) {
-            System.out.println(e.getClass().getName());
-            System.out.println(e.getMessage());
-            return null;
-        }
+    // Specify exception type
+    public Point encode(String address) throws Exception {
+        String uri = uriComponentDirector.getEncodeUri(address).toUriString();
+        ResponseEntity<Point> res = restTemplate.getForEntity(uri, Point.class);
+        if (res.getStatusCode().isError()) { throw new Exception("callout to Here API failed"); }
+        
+        return res.getBody();
     }
 
-    public Address decode(Point p) {
-        // Here URL builder class needed
-        // StringBuilder instead of manual string construction?
-        String service = HereApiService.REVERSE_GEOCODING.service;
-        String protocol = "https";
-        String host = service + "." + hereProperties.getBaseOrigin();
-        String path = hereProperties.getApiVersion() + "/" + service;
-        // find way to abstract query param names
-        String params = "at=" + p + "&apiKey=" + hereProperties.getApiKey();
-        String url = protocol + "://" + host + "/" + path + "?" + params;
+    // Specify exception type
+    public Address decode(Point point) throws Exception {
+        String uri = uriComponentDirector.getDecodeUri(point).toUriString();
+        ResponseEntity<Address> res = restTemplate.getForEntity(uri, Address.class);
+        if (res.getStatusCode().isError()) { throw new Exception("callout to Here API failed"); }
 
-        try {
-            ResponseEntity<Address> res = restTemplate.getForEntity(url, Address.class);
-            if (res.getStatusCode().isError()) { throw new Exception("callout to Here API failed"); }
-    
-            return res.getBody();
-        } catch (Exception e) {
-            System.out.println(e.getClass().getName());
-            System.out.println(e.getMessage());
-            return null;
-        }
+        return res.getBody();
     }
 }
