7cd2e3990cfb5e95238bb0be40eff94f6ff692b6
evanmpollack <evanmpollack@gmail.com>
2023-12-15 20:47:54 -0500

    first commit redesign main

---
+++ src/main/java/com/evanm/weather/WeatherApplication.java
@@ -0,0 +1,13 @@
+package com.evanm.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class WeatherApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+}
