63bea2f358c7f92678134389ec890f09fdcb3315
evanmpollack <evanmpollack@gmail.com>
2023-12-21 12:16:20 -0500

    api property files created --> may not be needed

--- src/main/java/com/evanm/weather/config/HereProperties.java
+++ src/main/java/com/evanm/weather/config/HereProperties.java
@@ -1,5 +1,34 @@
 package com.evanm.weather.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "here")
 public class HereProperties {
-    
+    private String apiKey;
+    private String apiVersion;
+    private String baseHost;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public String getBaseHost() {
+        return baseHost;
+    }
+
+    public void setBaseHost(String baseHost) {
+        this.baseHost = baseHost;
+    }
 }
--- src/main/java/com/evanm/weather/config/NWSProperties.java
+++ src/main/java/com/evanm/weather/config/NWSProperties.java
@@ -1,5 +1,25 @@
 package com.evanm.weather.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "nws")
 public class NWSProperties {
+    private String host;
+    private String userAgent;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
     
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
 }
