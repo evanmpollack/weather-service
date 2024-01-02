ee11bcc84c5ccf40d94b817ef0ce2621f4eb9855
evanmpollack <evanmpollack@gmail.com>
2024-01-02 18:19:20 -0500

    restructured to use default crud repository methods (look into naming convention for GeoRepository)

--- src/main/java/com/evanm/weather/repository/GeoRepository.java
+++ src/main/java/com/evanm/weather/repository/GeoRepository.java
@@ -2,8 +2,7 @@ package com.evanm.weather.repository;
 
 import java.util.Optional;
 import org.locationtech.jts.geom.Coordinate;
-import org.springframework.data.repository.CrudRepository;
 
-public interface GeoRepository<T, ID> extends CrudRepository<T, ID>{
-   Optional<T> findByCoordinate(Coordinate coordinate); 
+public interface GeoRepository<T> {
+   Optional<T> findOneByCoordinate(Coordinate coordinate); 
 }
