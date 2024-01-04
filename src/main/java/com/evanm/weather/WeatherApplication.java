7c91467ee6bc21465ebcef3c84a25efb9499dc72
evanmpollack <evanmpollack@gmail.com>
2024-01-03 20:26:45 -0500

    enabled redis repos

--- src/main/java/com/evanm/weather/WeatherApplication.java
+++ src/main/java/com/evanm/weather/WeatherApplication.java
@@ -3,9 +3,11 @@ package com.evanm.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableRedisRepositories
 public class WeatherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
