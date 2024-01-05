1bf9d9d7af0c67ad67fc433eab9c114d1e822041
evanmpollack <evanmpollack@gmail.com>
2024-01-05 17:11:57 -0500

    registered deserializers

--- src/main/java/com/evanm/weather/config/JacksonConfiguration.java
+++ src/main/java/com/evanm/weather/config/JacksonConfiguration.java
@@ -1,14 +1,26 @@
 package com.evanm.weather.config;
 
+import org.locationtech.jts.geom.Coordinate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.evanm.weather.domain.Address;
+import com.evanm.weather.serializers.AddressDeserializer;
+import com.evanm.weather.serializers.CoordinateDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Configuration
 public class JacksonConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Address.class, new AddressDeserializer());
+        module.addDeserializer(Coordinate.class, new CoordinateDeserializer());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(module);
+        
+        return mapper;
     }
 }
