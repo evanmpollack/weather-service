11417baafd50deadc763dc6935a6239a8081d8f9
evanmpollack <evanmpollack@gmail.com>
2024-01-10 20:25:50 -0500

    added java 8 date/time support to jackson

--- src/main/java/com/evanm/weather/config/JacksonConfiguration.java
+++ src/main/java/com/evanm/weather/config/JacksonConfiguration.java
@@ -6,24 +6,42 @@ import org.springframework.context.annotation.Configuration;
 
 import com.evanm.weather.domain.Address;
 import com.evanm.weather.domain.Alerts;
-import com.evanm.weather.serializers.AddressDeserializer;
-import com.evanm.weather.serializers.AlertsDeserializer;
-import com.evanm.weather.serializers.CoordinateDeserializer;
+import com.evanm.weather.domain.Forecast;
+import com.evanm.weather.serializers.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JacksonConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(Address.class, new AddressDeserializer());
-        module.addDeserializer(Coordinate.class, new CoordinateDeserializer());
-        module.addDeserializer(Alerts.class, new AlertsDeserializer());
-
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(module);
+        
+        // Module registration
+        mapper.registerModule(serializerModule());
+        mapper.registerModule(new JavaTimeModule());
+        
+        // Feature configuration
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         
         return mapper;
+    }
+
+    private SimpleModule serializerModule() {
+        SimpleModule serializerModule = new SimpleModule();
+        
+        // Serializers
+        serializerModule.addSerializer(Alerts.class, new AlertsSerializer());
+        serializerModule.addSerializer(Forecast.class, new ForecastSerializer());
+
+        // Deserializers
+        serializerModule.addDeserializer(Address.class, new AddressDeserializer());
+        serializerModule.addDeserializer(Coordinate.class, new CoordinateDeserializer());
+        serializerModule.addDeserializer(Alerts.class, new AlertsDeserializer());
+        serializerModule.addDeserializer(Forecast.class, new ForecastDeserializer());
+
+        return serializerModule;
     }
 }
