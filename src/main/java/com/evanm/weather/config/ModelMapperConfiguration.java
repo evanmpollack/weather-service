2e1f6df8c655ad9dea35122c678627d43c9948a2
evanmpollack <evanmpollack@gmail.com>
2024-01-10 20:32:39 -0500

    modelmapper init

--- src/main/java/com/evanm/weather/config/ModelMapperConfiguration.java
+++ src/main/java/com/evanm/weather/config/ModelMapperConfiguration.java
@@ -1,6 +1,7 @@
 package com.evanm.weather.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+            .setFieldMatchingEnabled(true)
+            .setFieldAccessLevel(AccessLevel.PRIVATE);
+        
+        return modelMapper;
     }
 }
